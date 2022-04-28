6465e54555e5f9baf78d8de009ca8c464516b28d
matt468779 <mengistusima@gmail.com>
2022-04-28 16:47:28 +0300

    first commit

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        answer = ["1"]
+        for i in range(2, n+1):
+            if i % 15 == 0:
+                answer.append("FizzBuzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return answer
---
+++ Grading Students.py
@@ -0,0 +1,12 @@
+def gradingStudents(grades):
+    # Write your code here
+    newGrades = []
+    for grade in grades:
+        if grade >= 38 and grade % 5 >= 3:
+            newGrades.append(grade + 5 - ( grade % 5))
+        else:
+            newGrades.append(grade)
+
+    return newGrades
+
+            
