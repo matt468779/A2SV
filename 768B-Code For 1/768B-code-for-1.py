0abb3bf85b1b0f076bc5ad17e8d8fe56b7e0ee22
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-05-17 02:05:15 +0300

    Create 768B-code-for-1.py

---
+++ 768B-Code For 1/768B-code-for-1.py
@@ -0,0 +1,38 @@
+from math import ceil
+
+def codeFor1(n):
+    if n == 1:
+        return []  
+    elif n == 0:
+        return [0]
+    return codeFor1(n//2) + [n%2]
+
+def numOfOne(arr, l, r):
+    if len(arr) == 1 and l == 1 and r == 1:
+        return arr[0]
+    l -= 1
+    if l % 2 == 0 or (r-1) % 2 == 0:
+        count = (r - l + 1) // 2
+    else:
+        count = (r - l - 1) // 2
+
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            continue
+        a0 = pow(2, i+1) - 1
+        if a0 >= r:
+            break
+        d = pow(2, i+2)
+        j = 0 if l <= a0 else ceil((l-a0)/d)
+        k = ceil((r-a0)/d)
+
+        count += k - j 
+    
+    return count
+
+inp = input().split(" ")
+n = int(inp[0])
+l = int(inp[1])
+r = int(inp[2])
+
+print(numOfOne(codeFor1(n), l, r))
