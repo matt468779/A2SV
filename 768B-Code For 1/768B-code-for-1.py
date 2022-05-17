0cd91e06c5249617c64db010a0d089877445449d
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-05-17 10:21:04 +0300

    Time: 46 ms, Space: 4400 KB  - CodeForces

--- 768B-Code For 1/768B-code-for-1.py
+++ 768B-Code For 1/768B-code-for-1.py
@@ -2,7 +2,7 @@ from math import ceil
 
 def codeFor1(n):
     if n == 1:
-        return []  
+        return [1]  
     elif n == 0:
         return [0]
     return codeFor1(n//2) + [n%2]
@@ -11,18 +11,14 @@ def numOfOne(arr, l, r):
     if len(arr) == 1 and l == 1 and r == 1:
         return arr[0]
     l -= 1
-    if l % 2 == 0 or (r-1) % 2 == 0:
-        count = (r - l + 1) // 2
-    else:
-        count = (r - l - 1) // 2
-
+    count = 0
     for i in range(len(arr)):
         if arr[i] == 0:
             continue
-        a0 = pow(2, i+1) - 1
+        a0 = pow(2, i) - 1
         if a0 >= r:
             break
-        d = pow(2, i+2)
+        d = pow(2, i+1)
         j = 0 if l <= a0 else ceil((l-a0)/d)
         k = ceil((r-a0)/d)
 
