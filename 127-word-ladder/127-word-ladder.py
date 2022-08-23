52de60015cf22129b6fe2be556827c8723e17c9e
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-08-23 19:00:23 +0300

    Time: 940 ms (17.86%), Space: 15.3 MB (64.93%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,27 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        words = set(wordList)
+        q = set([beginWord])
+        q2 = set()
+        visited = set()
+        res = 0
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                popped = q.pop()
+                if popped in visited:
+                    continue
+                if popped == endWord:
+                    return res
+                for i in range(len(popped)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        temp = popped[:i] + j + popped[i+1:]
+                        if temp not in visited and temp in words and temp not in q:
+                            q2.add(temp)
+                visited.add(popped)
+            q = q2
+            q2 = set()
+        return 0
