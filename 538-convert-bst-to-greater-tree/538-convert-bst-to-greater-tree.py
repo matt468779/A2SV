23f2838d42914e700bb9a9711bb05b7adb662541
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-08-30 16:26:41 +0300

    Time: 114 ms (62.82%), Space: 16.9 MB (22.87%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -6,19 +6,13 @@
 #         self.right = right
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.preSum = [0]
+        self.preSum = 0
         def dfs1(node):
             if node:
                 dfs1(node.right)
-                self.preSum.append(self.preSum[-1]+node.val)
+                self.preSum += node.val
+                node.val = self.preSum
                 dfs1(node.left)
-        
-        def dfs2(node):
-            if node:
-                dfs2(node.left)
-                node.val = self.preSum.pop()
-                dfs2(node.right)
                 
         dfs1(root)
-        dfs2(root)
         return root
