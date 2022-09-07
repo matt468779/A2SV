cdcb759dfc00e157e07c1395db9bc4f18e657454
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-09-07 21:10:50 +0300

    Time: 188 ms (5.13%), Space: 14.9 MB (62.68%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,16 +1,25 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        def traverse(i, j, dist):
-            if self.dp[i][j] != 'x':
-                return self.dp[i][j]
-            self.dp[i][j] = triangle[i][j] + min(traverse(i+1, j, dist+triangle[i][j]), traverse(i+1, j+1, dist+triangle[i][j]))
-            return self.dp[i][j]
+#         def traverse(i, j, dist):
+#             if self.dp[i][j] != 'x':
+#                 return self.dp[i][j]
+#             self.dp[i][j] = triangle[i][j] + min(traverse(i+1, j, dist+triangle[i][j]), traverse(i+1, j+1, dist+triangle[i][j]))
+#             return self.dp[i][j]
         
-        self.dp = []
-        for i in range(len(triangle)):
-            self.dp.append([])
-            for j in range(i+1):
-                self.dp[-1].append('x')
-        self.dp[-1] = triangle[-1]
+#         self.dp = []
+#         for i in range(len(triangle)):
+#             self.dp.append([])
+#             for j in range(i+1):
+#                 self.dp[-1].append('x')
+#         self.dp[-1] = triangle[-1]
     
-        return traverse(0, 0, triangle[0][0])
+#         return traverse(0, 0, triangle[0][0])
+    
+        dp = [triangle[0][0]] * len(triangle)
+        for i in range(1, len(triangle)):
+            triangle[i][i] += triangle[i-1][i-1]
+            triangle[i][0] += triangle[i-1][0]
+            for j in range(1, i):
+                triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1])
+        
+        return min(triangle[-1])
