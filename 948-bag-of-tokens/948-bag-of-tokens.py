74d02103b5ac64466fabba1ad09acd19c2afbaab
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-09-12 14:37:41 +0300

    Time: 58 ms (91.98%), Space: 13.9 MB (77.36%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -2,13 +2,15 @@ class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         tokens = collections.deque(sorted(tokens))
         res, score = 0, 0
-        while tokens and (score or power >= tokens[0]):
+        while tokens:
             if power >= tokens[0]:
                 power -= tokens.popleft()
                 score += 1
             elif score > 0:
                 power += tokens.pop()
                 score -= 1
+            else:
+                break
             res = max(res, score)
 
         return res
