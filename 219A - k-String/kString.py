d37e07b5ad65685ad3a96a23bb8aca8407136b68
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-12-29 09:11:56 +0300

    CodeForces

---
+++ 219A - k-String/kString.py
@@ -0,0 +1,18 @@
+def k_string():
+   k = int(input())
+   word = input()
+
+   wordCount = {}
+   for w in word:
+      wordCount[w] = wordCount.get(w, 0) + 1
+   
+   res = ''
+   for key, value in wordCount.items():
+      if value % k:
+         return -1
+   
+      res += key*(value//k)
+   
+   return res*k
+
+print(k_string())
