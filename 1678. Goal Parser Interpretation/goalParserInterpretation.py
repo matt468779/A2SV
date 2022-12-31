26f4616852eb7bc44df53bc141fa86469e955c1f
matt468779 <47981856+matt468779@users.noreply.github.com>
2022-12-31 12:42:00 +0300

    Runtime 38 ms Beats 68.10% Memory 13.9 MB Beats 54.83%

---
+++ 1678. Goal Parser Interpretation/goalParserInterpretation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        res, i = [], 0
+        while i < len(command):
+            if command[i] == 'G':
+                res.append('G')
+                i += 1
+            elif command[i:i+2] == '()':
+                res.append('o')
+                i += 2
+            else:
+                res.append('al')
+                i += 4
+
+        return ''.join(res)
