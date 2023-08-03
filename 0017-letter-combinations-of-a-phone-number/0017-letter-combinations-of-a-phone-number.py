a51ab8ca8152c990312812b802b66c372fe00add
matt468779 <47981856+matt468779@users.noreply.github.com>
2023-08-03 21:57:19 +0300

    Time: 34 ms (96.91%), Space: 16.3 MB (86.05%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        mapping = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+        res = collections.deque(mapping[int(digits[0])])
+        for n in digits[1:]:
+            l = len(res)
+            s = mapping[int(n)]
+            for i in range(l):
+                now = res.popleft()
+                for nex in s:
+                    res.append(now+nex)
+        
+        return list(res)
